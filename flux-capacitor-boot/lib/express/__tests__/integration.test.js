f649c13d27b1708c2263de88493304c92eabf65c
Andy Wermke <andy@dev.next-step-software.com>
2016-10-29 17:50:00 +0200

    Make bootstrapping in integration tests more explicit

--- flux-capacitor-boot/lib/express/__tests__/integration.test.js
+++ flux-capacitor-boot/lib/express/__tests__/integration.test.js
@@ -13,17 +13,25 @@ const commands = {
 }
 
 test(`bootstrapping a server from scratch`, async (t) => {
-  const server = await bootstrapServer(mockStore())
+  const server = await bootstrap([
+    use.app(createExpressApp()),
+    use.store(mockStore())
+  ]).listen()
 
   t.is(server.listening, true)
   server.close()
 })
 
 test(`dispatching 'addUser' works`, async (t) => {
-  const server = await bootstrapServer(mockStore())
-  const { port } = server.address()
+  const server = await bootstrap([
+    use.app(createExpressApp()),
+    use.store(mockStore()),
+    use.route('/dispatch/:commandName', createDispatcher(commands))
+  ]).listen()
 
+  const { port } = server.address()
   const events = await post(`http://localhost:${port}/dispatch/addUser`, { name: 'Nancy Newuser' })
+
   t.deepEqual(events, [
     { type: 'addUser', name: 'Nancy Newuser' }
   ])
@@ -32,19 +40,42 @@ test(`dispatching 'addUser' works`, async (t) => {
 })
 
 test(`dispatching 'xyUser' fails`, async (t) => {
-  const server = await bootstrapServer(mockStore())
-  const { port } = server.address()
+  const server = await bootstrap([
+    use.app(createExpressApp()),
+    use.store(mockStore()),
+    use.route('/dispatch/:commandName', createDispatcher(commands))
+  ]).listen()
 
+  const { port } = server.address()
   await t.throws(post(`http://localhost:${port}/dispatch/xyUser`, {}), /Unknown command: xyUser/)
 
   server.close()
 })
 
-test(`reading events works`, async (t) => {
-  const server = await bootstrapServer(mockStore())
-  const { port } = server.address()
+test(`dispatching a restricted command fails`, async (t) => {
+  const authorizer = (req) => req.params.command === 'restricted' ? authorize.deny('Nope!') : authorize.allow()
+  const server = await bootstrap([
+    use.app(createExpressApp()),
+    use.store(mockStore()),
+    use.route('/dispatch/:command', createDispatcher(commands), authorizer)
+  ]).listen()
 
+  const { port } = server.address()
+  await t.throws(post(`http://localhost:${port}/dispatch/restricted`, {}), /Nope!/)
+
+  server.close()
+})
+
+test(`reading events works`, async (t) => {
+  const server = await bootstrap([
+    use.app(createExpressApp()),
+    use.store(mockStore()),
+    use.route('/events/:id?', createReadRoute('events', { sortBy: 'timestamp', sortOrder: 'DESC' }))
+  ]).listen()
+
+  const { port } = server.address()
   const events = await request(`http://localhost:${port}/events`)
+
   t.deepEqual(events, JSON.stringify([
     { id: '123', type: 'mockEvent', foo: 'bar' }
   ]))
@@ -53,10 +84,15 @@ test(`reading events works`, async (t) => {
 })
 
 test(`reading a single event works`, async (t) => {
-  const server = await bootstrapServer(mockStore())
-  const { port } = server.address()
+  const server = await bootstrap([
+    use.app(createExpressApp()),
+    use.store(mockStore()),
+    use.route('/events/:id?', createReadRoute('events', { sortBy: 'timestamp', sortOrder: 'DESC' }))
+  ]).listen()
 
+  const { port } = server.address()
   const events = await request(`http://localhost:${port}/events/123`)
+
   t.deepEqual(events, JSON.stringify(
     { id: '123', type: 'mockEvent', foo: 'bar' }
   ))
@@ -67,15 +103,18 @@ test(`reading a single event works`, async (t) => {
 test(`websocket propagates dispatched events`, async (t) => {
   t.plan(3)
 
+  const store = mockStore()
   const authorizer = sinon.spy(
     (event) => event.type === 'restrictedEvent' ? authorize.deny() : authorize.allow()
   )
-  const wsMiddleware = use.route('/websocket', createWebSocket(authorizer))
 
-  const store = mockStore()
-  const server = await bootstrapServer(store, [ wsMiddleware ])
+  const server = await bootstrap([
+    use.app(createExpressApp()),
+    use.store(store),
+    use.route('/websocket', createWebSocket(authorizer))
+  ]).listen()
+
   const { port } = server.address()
-
   const ws = new WebSocket(`ws://localhost:${port}/websocket`)
 
   await new Promise((resolve) =>
@@ -160,17 +199,6 @@ function mockCollection (name, rows) {
   const findById = sinon.spy(async (id) => rows.find((row) => row.id === id))
 
   return { name, fields, findAll, findById }
-}
-
-function bootstrapServer (store, additionalMiddleware = []) {
-  return bootstrap([
-    // ({ app, routes, store }) => Promise.resolve({ app, routes, store }),
-    use.app(createExpressApp()),
-    use.store(store),
-    use.route('/dispatch/:commandName', createDispatcher(commands)/* , (req) => authorize.allow() */),
-    use.route('/events/:id?', createReadRoute('events', { sortBy: 'timestamp', sortOrder: 'DESC' })),
-    use.route('/users/:id?', createReadRoute('users'))
-  ].concat(additionalMiddleware)).listen()
 }
 
 function post (uri, body) {
