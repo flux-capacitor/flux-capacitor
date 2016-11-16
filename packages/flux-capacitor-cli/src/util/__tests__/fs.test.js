aeffc3ec0b9986316a1846f78afe794431534a29
Andy Wermke <andy@dev.next-step-software.com>
2016-11-16 17:47:34 +0100

    Implement `locatePackageJson()`

--- packages/flux-capacitor-cli/src/util/__tests__/fs.test.js
+++ packages/flux-capacitor-cli/src/util/__tests__/fs.test.js
@@ -1,6 +1,7 @@
 import test from 'ava'
+import fs from 'mz/fs'
 import path from 'path'
-import { recursiveFileList } from '../fs'
+import { locatePackageJson, recursiveFileList } from '../fs'
 
 test('recursiveFileList()', async (t) => {
   const files = await recursiveFileList(path.resolve(__dirname, 'fixtures', 'template'))
@@ -8,4 +9,22 @@ test('recursiveFileList()', async (t) => {
   t.deepEqual(files, [
     'index.js', 'foo/bar.js', 'foo/baz.txt'
   ])
+})
+
+test('locatePackageJson() works', async (t) => {
+  const expectedPath = fs.realpathSync(path.resolve(__dirname, '..', '..', '..', 'package.json'))
+
+  t.is(await locatePackageJson(), expectedPath)
+})
+
+test('locatePackageJson("/") fails properly', async (t) => {
+  t.throws(locatePackageJson('/'), 'package.json file not found.')
+})
+
+test('locatePackageJson("/") fails properly', async (t) => {
+  await t.throws(locatePackageJson('/'), 'package.json file not found.')
+})
+
+test('locatePackageJson("/tmp") fails properly', async (t) => {
+  await t.throws(locatePackageJson('/tmp'), 'package.json file not found.')
 })
--- packages/flux-capacitor-cli/src/util/fs.js
+++ packages/flux-capacitor-cli/src/util/fs.js
@@ -1,7 +1,9 @@
 import fs from 'mz/fs'
+import exists from 'path-exists'
 import path from 'path'
 
 export {
+  locatePackageJson,
   recursiveFileList
 }
 
@@ -29,4 +31,24 @@ async function recursiveFileList (dirPath) {
   )
 
   return fileNames.concat(subDirFiles)
+}
+
+/**
+ * Searches the given directory and all its parent directories for a `package.json`.
+ *
+ * @param {string} dirPath
+ * @return {Promise<string>} Resoles to the path to the `package.json` or rejects.
+ */
+async function locatePackageJson (dirPath = process.cwd()) {
+  const realPath = path.isAbsolute(dirPath) ? dirPath : await fs.realpath(dirPath)
+  const parentPath = path.dirname(realPath)
+  const filePath = path.join(realPath, 'package.json')
+
+  if (await exists(filePath)) {
+    return filePath
+  } else if (parentPath !== realPath) {
+    return await locatePackageJson(parentPath)
+  } else {
+    throw new Error(`package.json file not found.`)
+  }
 }
